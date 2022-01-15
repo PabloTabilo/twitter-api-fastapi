ff34a4a2c933add301c8fbab54b7e7cfa27a8b61
PabloTabilo <pablo_t_95@hotmail.com>
2022-01-15 17:22:29 -0300

    models_tweets

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Python
 from uuid import UUID
 from datetime import date
+from datetime import datetime
 from typing import Optional
 
 # Pydantic
@@ -24,7 +25,8 @@ class UserBase(BaseModel):
 class UserLogin(UserBase):
     password : str = Field(
             ...,
-            min_length=8
+            min_length=8,
+            max_length=64
             )
 
 
@@ -42,9 +44,15 @@ class User(UserBase):
     birth_date : Optional[date] = Field(default=None)
 
 
-
 class Tweets(BaseModel):
-    pass
+    tweet_id : UUID = Field(...)
+    content : str = Field(..., 
+            min_length=1,
+            max_length=256
+            )
+    created_at : datetime = Field(default=datetime.now())
+    updated_at : Optional[datetime] = Field(default=None)
+    by : User = Field(...)
 
 @app.get(path="/")
 def home():
